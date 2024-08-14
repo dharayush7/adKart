be9fed4b3af709dc8234b84142222efadb2482dd
AYUSH DHAR <ayushdh2@gmail.com>
2024-08-14 14:12:17 +0530

    raedy to git push

--- shop/static/script/script.js
+++ shop/static/script/script.js
@@ -1,16 +1,15 @@
 let cartLink = document.getElementById("cartLink");
-let pop = document.getElementById("pop")
-let body = document.querySelector('.popover-body')
+let pop = document.getElementById("pop");
+let body = document.querySelector(".popover-body");
 
-cart = localStorage.getItem('cart')
+cart = localStorage.getItem("cart");
 
-if(cart){
-    jsonCart = JSON.parse(cart);
-    cartLink.innerHTML = `(${Object.keys(jsonCart).length})`
+if (cart) {
+  jsonCart = JSON.parse(cart);
+  cartLink.innerHTML = `(${Object.keys(jsonCart).length})`;
 }
 
+const height = window.innerHeight;
+const mainHeight = height - 180;
 
-
-
-
-
+document.querySelector("main").style = `min-height: ${mainHeight}px`;
